a6af9e52a4a6e439cc9946362637a9d86a10a610
tomodall <129076985+tomodall@users.noreply.github.com>
2023-04-12 17:53:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
-let display = document.getElementById('display');
-
-function addToDisplay(value) {
-	display.value += value;
-}
-
-function clearDisplay() {
-	display.value = '';
-}
-
-function calculate() {
-	try {
-		display.value = eval(display.value);
-	} catch (error) {
-		display.value = 'Error';
-	}
-}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js"></script>
+<script>
+  const downloadBtn = document.querySelector('#download-btn');
+  
+  downloadBtn.addEventListener('click', () => {
+    const fileURL = '파일경로/파일이름.pdf';
+    const fileName = '파일이름.pdf';
+    saveAs(fileURL, fileName);
+  });
+</script>
